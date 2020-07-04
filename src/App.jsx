514d42011024a5ed5d055c1263ffacd2d0763a9f
Mihai Timoficiuc <52246177+MTimo27@users.noreply.github.com>
2020-07-04 16:58:16 +0300

    Initial Commit-Navbar Added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Heading from './components/common/Heading';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Heading />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/common/Heading.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import './styles/Heading.css';
+
+function Heading() {
+  return (
+    <React.Fragment>
+      <section id="nav-bar">
+        <Navbar bg="light" expand="sm">
+          <Navbar.Brand href="/">
+            <img src="img/Logo Zece La Informatica.png" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#top">Acasa</Nav.Link>
+              <Nav.Link href="/">Modulul 1</Nav.Link>
+              <NavDropdown title="Modulul 2" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/">Clasa a IX-a</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/">Clasa a X-a</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/">Clasa a XI-a</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="/">Modulul 3</Nav.Link>
+              <Nav.Link href="/">Despre noi</Nav.Link>
+              <Nav.Link href="/">Referinte</Nav.Link>
+              <Nav.Link href="/">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default Heading;
